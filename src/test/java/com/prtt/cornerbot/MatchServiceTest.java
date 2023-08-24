06f6f66db55630654d8282b5e418e7510ebb5295
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2023-08-23 23:48:24 -0300

    feat: add unit tests to project

---
+++ src/test/java/com/prtt/cornerbot/MatchServiceTest.java
@@ -0,0 +1,63 @@
+package com.prtt.cornerbot;
+
+import com.prtt.cornerbot.domain.DangerousAttacksPerMinute;
+import com.prtt.cornerbot.domain.Match;
+import com.prtt.cornerbot.domain.PressureStats;
+import com.prtt.cornerbot.domain.Scores;
+import com.prtt.cornerbot.service.MatchService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class MatchServiceTest {
+
+
+    @MockBean
+    private MatchService matchService;
+
+    @Test
+    public void testIsDifferenceOne_True() {
+        Match match = createMatchWithScores(3L, 4L);
+        assertTrue(matchService.isMatchOnFilter(match));
+    }
+
+    @Test
+    public void testIsDifferenceOne_False() {
+        Match match = createMatchWithScores(2L, 5L);
+        assertFalse(matchService.isMatchOnFilter(match));
+    }
+
+    @Test
+    public void testIsAvgAppmHigh_True() {
+        Match match = createMatchWithAppm(2.0, 1.5);
+        assertTrue(matchService.isMatchOnFilter(match));
+    }
+
+    @Test
+    public void testIsAvgAppmHigh_False() {
+        Match match = createMatchWithAppm(1.0, 0.5);
+        assertFalse(matchService.isMatchOnFilter(match));
+    }
+
+    private Match createMatchWithScores(Long homeScore, Long awayScore) {
+        Scores scores = new Scores(homeScore, awayScore);
+        PressureStats pressureStats = new PressureStats(new DangerousAttacksPerMinute(1.0, 1.0), new DangerousAttacksPerMinute(2.0, 2.0));
+        Match match = new Match();
+        match.setScores(scores);
+        match.setPressureStats(pressureStats);
+        return match;
+    }
+
+    private Match createMatchWithAppm(Double homeAppm, Double awayAppm) {
+        Scores scores = new Scores(2L, 2L);
+        PressureStats pressureStats = new PressureStats(new DangerousAttacksPerMinute(homeAppm, awayAppm), new DangerousAttacksPerMinute(2.0, 2.0));
+        Match match = new Match();
+        match.setScores(scores);
+        match.setPressureStats(pressureStats);
+        return match;
+    }
+}
