24d248452ea467b5b35926a0c9849b4c316b39b6
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2023-09-04 21:08:20 -0300

    fix: fix params off the corner filter method

--- src/main/java/com/prtt/cornerbot/service/MatchService.java
+++ src/main/java/com/prtt/cornerbot/service/MatchService.java
@@ -49,9 +49,9 @@ public class MatchService {
 
     private boolean isMatchOnAppmHigh(Match match){
         return (match.getPressureStats().getAppm1().getHome() >= 1.05 &&
-                Math.abs(match.getScores().getAwayTeamScore() - match.getScores().getHomeTeamScore()) <= 1) ||
+                Math.abs(match.getScores().getAwayTeamScore() - match.getScores().getHomeTeamScore()) < 2) ||
                 (match.getPressureStats().getAppm1().getAway() >= 1.05 &&
-                        Math.abs(match.getScores().getHomeTeamScore() - match.getScores().getAwayTeamScore()) <= 1);
+                        Math.abs(match.getScores().getHomeTeamScore() - match.getScores().getAwayTeamScore()) < 2);
     }
 
 
