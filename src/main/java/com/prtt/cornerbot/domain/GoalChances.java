35c0224881803d8e95b4e736fa2031491910bb36
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2023-09-11 23:03:25 -0300

    feature: new json class

---
+++ src/main/java/com/prtt/cornerbot/domain/GoalChances.java
@@ -0,0 +1,11 @@
+package com.prtt.cornerbot.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class GoalChances {
+    private Long home;
+    private Long away;
+}
