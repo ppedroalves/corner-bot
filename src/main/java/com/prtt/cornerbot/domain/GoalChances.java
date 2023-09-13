235a3bbe21062c86d318c996e019de4d40973b35
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2023-09-12 22:42:21 -0300

    fix: add no args constructor

--- src/main/java/com/prtt/cornerbot/domain/GoalChances.java
+++ src/main/java/com/prtt/cornerbot/domain/GoalChances.java
@@ -2,9 +2,11 @@ package com.prtt.cornerbot.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class GoalChances {
     private Long home;
     private Long away;
