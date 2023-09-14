32a75e0ef34777a5532d5b07da837f0521a21a29
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2023-09-13 22:06:22 -0300

    feat: enable cache

--- src/main/java/com/prtt/cornerbot/CornerBotApplication.java
+++ src/main/java/com/prtt/cornerbot/CornerBotApplication.java
@@ -2,12 +2,14 @@ package com.prtt.cornerbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
+@EnableCaching
 public class CornerBotApplication {
 
 	public static void main(String[] args) {
